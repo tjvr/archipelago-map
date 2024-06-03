8e27223cacd3725490aea95a64cd59cf1942ecbc
Tim Radvan <tim@tjvr.org>
2024-06-03 12:22:30 +0100

    better svg tiler

--- svg_tiler.py
+++ svg_tiler.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 import shlex
 import subprocess
+import sys
 
 source = 'tmp/1953mapv1.opt.svg'
 x_min, y_min = 755, 1511
@@ -9,12 +10,18 @@ x_max, y_max = 9827, 10583
 print(f"{x_min}:{y_min}:{x_max}:{y_max}")
 
 # At zoom level 0 there is one tile, etc.
+min_zoom, max_zoom = 6,6
 
-for z in range(2):
+for z in range(min_zoom, max_zoom+1):
     steps = 2 ** z
     print(f"zoom level {z}: {steps} steps, {steps*steps} tiles")
+
+    actions = [
+        f"file-open:{source}",
+    ]
+    Path(f"tiles2/{z}/").mkdir(parents=True, exist_ok=True)
+
     for y in range(steps):
-        Path(f"tiles/{z}/{y}/").mkdir(parents=True, exist_ok=True)
         for x in range(steps):
             x0 = int(x_min + x*(x_max-x_min)/steps)
             x1 = int(x_min + (x+1)*(x_max-x_min)/steps)
@@ -22,15 +29,28 @@ for z in range(2):
             y0 = int(y_min + y*(y_max-y_min)/steps)
             y1 = int(y_min + (y+1)*(y_max-y_min)/steps)
 
-            args = [
-                'inkscape', 
-                source,
-                '--export-area',
-                f"{x0}:{y0}:{x1}:{y1}",
-                '--export-filename',
-                f"tiles/{z}/{y}/{x}.png"
+            out_width = 512
+            out_height = 512
+
+            actions += [
+                # element-image-crop causes Inkscape to crash.
+                f"export-area:{x0}:{y0}:{x1}:{y1}",
+                f"export-filename:tiles2/{z}/{x}-{y}.png",
+                f"export-width:{out_width}",
+                f"export-height:{out_height}",
+                "export-do",
             ]
-            print(" ".join(map(shlex.quote, args)))
-            p = subprocess.Popen(args)
-            p.wait()
+
+    # Each inkscape command must be under ~100K bytes, which is around 5000
+    # actions, or ~1000 tiles.
+    command = [
+        'inkscape',
+        '--actions',
+        "; ".join(actions),
+    ]
+    #print(" ".join(map(shlex.quote, command)))
+    print(len(actions))
+    print(len(command[2]))
+    p = subprocess.Popen(command)
+    p.wait()
 
